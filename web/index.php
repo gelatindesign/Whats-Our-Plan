9d82aa006d2153b3df9137c13a21ddb9276549a9
Laurence Roberts <info@gelatindesign.co.uk>
2012-10-27 03:39:14 +0100

    Fixed htaccess & router

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require '../vendor/autoload.php';
 
 Ares\Config::$root = dirname(__DIR__);
 
+// Attempt to start the request
 try {
 	Ares\Request::start();
 
@@ -13,4 +14,11 @@ try {
 	
 }
 
-Ares\Router::find();
+// Attempt to find the url
+try {
+	Ares\Router::find();
+
+} catch (Exception $e) {
+	echo $e->getMessage();
+	exit;
+}
